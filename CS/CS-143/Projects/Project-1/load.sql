468f8dc10aa7539ff0c1d5836e297fbf0cc64d49
Charles Zhang <charleszhangmb@gmail.com>
2021-09-29 19:01:31 -0700

    09/29/21 update | CS 143: Project 1 load script

--- CS/CS-143/Projects/Project-1/load.sql
+++ CS/CS-143/Projects/Project-1/load.sql
@@ -1,11 +1,11 @@
 -- Load Actor data
-LOAD DATA LOCAL INFILE 'actor1.del' INTO TABLE Actor;
-LOAD DATA LOCAL INFILE 'actor2.del' INTO TABLE Actor;
-LOAD DATA LOCAL INFILE 'actor3.del' INTO TABLE Actor;
+LOAD DATA LOCAL INFILE './actor1.del' INTO TABLE Actor;
+LOAD DATA LOCAL INFILE './actor2.del' INTO TABLE Actor;
+LOAD DATA LOCAL INFILE './actor3.del' INTO TABLE Actor;
 -- Load Movie data
-LOAD DATA LOCAL INFILE 'movie.del' INTO TABLE Movie;
+LOAD DATA LOCAL INFILE './movie.del' INTO TABLE Movie;
 -- Load MovieActor data
-LOAD DATA LOCAL INFILE 'movieactor1.del' INTO TABLE MovieActor;
-LOAD DATA LOCAL INFILE 'movieactor2.del' INTO TABLE MovieActor;
+LOAD DATA LOCAL INFILE './movieactor1.del' INTO TABLE MovieActor;
+LOAD DATA LOCAL INFILE './movieactor2.del' INTO TABLE MovieActor;
 -- Load MovieGenre data
-LOAD DATA LOCAL INFILE 'moviegenre.del' INTO TABLE MovieGenre;
+LOAD DATA LOCAL INFILE './moviegenre.del' INTO TABLE MovieGenre;
