49fa888af48a33f5d16e58ede4ab12dc5daa0a4b
Charles Zhang <charleszhangmb@gmail.com>
2021-10-16 12:19:10 -0700

    10/16/21 update | CS 143: Project 2 review page

--- CS/CS-143/Projects/Project-2/Code/pages/review.php
+++ CS/CS-143/Projects/Project-2/Code/pages/review.php
@@ -23,14 +23,14 @@
 
 <div id="content">
   <?php
-    $id = $_GET['id'];
+    $id = $_REQUEST['id'];
+    $mid = $_POST['mid'];
     // Check if default page
     if ($id) {
       // Connect to class_db
       $db = new mysqli('localhost', 'cs143', '', 'class_db');
-      if ($db->connect_errno > 0) { 
-        die('Unable to connect to database [' . $db->connect_error . ']'); 
-      }
+      if ($db->connect_errno > 0)
+        die('Unable to connect to database [' . $db->connect_error . ']');
       // Query Movie using ID
       $query = "SELECT title
                 FROM Movie
@@ -38,9 +38,10 @@
       $res = $db->query($query);
       $row = $res->fetch_assoc();
       $title = $row['title'];
-      echo "<form action='review.php' method='get'>";
+      // Render form
+      echo "<form action='./review.php' method='post'>";
       echo  "<p id='page-header'>Write a Review for <em>$title</em>!</p>";
-      echo  "<input id='hide' name='id' value=$id />";
+      echo  "<input id='hide' name='mid' value=$id />";
       echo  "<label for='name-field'>Your name</label>";
       echo  "<input id='name-field' name='name' placeholder='Joe Bruin' />";
       echo  "<label for='rating-field'>Your rating</label>";
@@ -55,14 +56,47 @@
       echo  "<label for='comment-field'>Your comment</label>";
       echo  "<textarea id='comment-field' name='comment' placeholder='Write a comment'></textarea>";
       echo  "<input type='submit' value='Submit!' />";
-      // Check if review is beling posted
-      if (array_key_exists("name", $_GET) && array_key_exists("rating", $_GET) && array_key_exists("comment", $_GET)) {
-        // Create insert query
-        $name = $_GET['name'];
-        $rating = $_GET['rating'];
-        $comment = $_GET['comment'];
+    // Review submitted
+    } else if ($mid) {
+      // Connect to class_db
+      $db = new mysqli('localhost', 'cs143', '', 'class_db');
+      if ($db->connect_errno > 0)
+        die('Unable to connect to database [' . $db->connect_error . ']');
+      // Query Movie using ID
+      $query = "SELECT title
+                FROM Movie
+                WHERE id = $mid";
+      $res = $db->query($query);
+      $row = $res->fetch_assoc();
+      $title = $row['title'];
+      // Render form
+      echo "<form action='./review.php' method='post'>";
+      echo  "<p id='page-header'>Write a Review for <em>$title</em>!</p>";
+      echo  "<input id='hide' name='mid' value=$mid />";
+      echo  "<label for='name-field'>Your name</label>";
+      echo  "<input id='name-field' name='name' placeholder='Joe Bruin' />";
+      echo  "<label for='rating-field'>Your rating</label>";
+      echo  "<select id='rating-field' name='rating'>";
+      echo    "<option value='' disabled>Select</option>";
+      echo    "<option value='1'>1</option>";
+      echo    "<option value='2'>2</option>";
+      echo    "<option value='3'>3</option>";
+      echo    "<option value='4'>4</option>";
+      echo    "<option value='5'>5</option>";
+      echo  "</select>";
+      echo  "<label for='comment-field'>Your comment</label>";
+      echo  "<textarea id='comment-field' name='comment' placeholder='Write a comment'></textarea>";
+      echo  "<input type='submit' value='Submit!' />";
+      // Create insert query
+      $name = $_POST['name'];
+      $rating = $_POST['rating'];
+      $comment = $_POST['comment'];
+      // Protect name field
+      if (strlen($name) > 20)
+        echo "<p>Error: Name too long</p>";
+      else {
         $query = "INSERT INTO Review
-                  VALUES ('$name', NOW(), $id, $rating, '$comment')";
+                  VALUES ('$name', NOW(), $mid, $rating, '$comment')";
         $db->query($query);
         echo "<p>Your review has been submitted!</p>";
       }
@@ -71,6 +105,7 @@
       $res->free();
       // Close connection
       $db->close();
+    // Bad URL
     } else {
       echo '<div id="page-header">That page doesn\'t exist!</div>';
     }
