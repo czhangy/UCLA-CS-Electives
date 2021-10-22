563cd9fd4c8b4cb0237e91ff7b18c47f5ca737d8
Charles Zhang <charleszhangmb@gmail.com>
2021-10-22 16:59:39 -0700

    10/22/21 update | CS 143: Homework 3 query 15

--- CS/CS-143/Homework/Homework-3/Submission/q15.sql
+++ CS/CS-143/Homework/Homework-3/Submission/q15.sql
@@ -2,6 +2,6 @@ WITH Credits AS (SELECT stud_id, SUM(credits) AS credits
                  FROM Takes, Class
                  WHERE Takes.class_id = Class.id
                  GROUP BY stud_id)
-SELECT stud_id, tot_cred - COALESCE(credits, 0) AS credit_discrepency
-FROM Student, Credits
-WHERE id = stud_id;
+SELECT id AS stud_id, tot_cred - COALESCE(credits, 0) AS credit_discrepency
+FROM Student LEFT OUTER JOIN Credits
+ON id = stud_id;
