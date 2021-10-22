d0e8a42e467f92240f12038fac5a6f56d7efa63a
Charles Zhang <charleszhangmb@gmail.com>
2021-10-22 16:51:19 -0700

    10/22/21 update | CS 143: Homework 3 query 13

--- CS/CS-143/Homework/Homework-3/Submission/q13.sql
+++ CS/CS-143/Homework/Homework-3/Submission/q13.sql
@@ -1,4 +1,10 @@
-SELECT id
-FROM Student
-ORDER BY tot_cred DESC
+WITH Credits AS (
+                    SELECT stud_id, SUM(credits) AS credits
+                    FROM Class, Takes
+                    WHERE class_id = id
+                    GROUP BY stud_id
+                )
+SELECT stud_id
+FROM Credits
+ORDER BY credits DESC
 LIMIT 4;
