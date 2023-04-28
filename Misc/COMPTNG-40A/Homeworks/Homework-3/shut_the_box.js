74a7a054b958105a66425841ec35a80c4e943b33
Charles Zhang <charleszhangmb@gmail.com>
2023-04-28 16:10:05 -0700

    04/28/23 update | COMPTNG 40A: Homework 3 HTML

--- Misc/COMPTNG-40A/Homeworks/Homework-3/shut_the_box.js
+++ Misc/COMPTNG-40A/Homeworks/Homework-3/shut_the_box.js
@@ -1,39 +1,68 @@
-let unchecked_total = 45; // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+let unchecked_total = 45;
 
 let dice_roll = null;
-// const dice_result;
 
-// const roll_dice_btn;
-// const submit_btn;
-// const finish_btn;
+const roll_dice_btn = document.getElementById("roll-dice");
+const submit_btn = document.getElementById("submit");
+const finish_btn = document.getElementById("give-up");
+const roll_result = document.getElementById("roll-result");
 
-// const boxes;
-// const checkboxes;
+const boxes = document.getElementsByTagName("th");
+const checkboxes = document.getElementsByTagName("input");
 
-// Add 12 event listeners.
+for (let i = 0; i < boxes.length; i++) {
+	boxes[i].addEventListener("click", () => {
+		checkboxes[i].checked = !checkboxes[i].checked;
+	});
+}
 
+roll_dice_btn.addEventListener("click", roll_dice);
+submit_btn.addEventListener("click", submit);
+finish_btn.addEventListener("click", finish);
 
 function roll_dice() {
-  // Roll dice, inject text, disable / enable buttons.
+	dice_roll = Math.floor(Math.random() * 6) + 1;
+	if (unchecked_total > 6) {
+		dice_roll += Math.floor(Math.random() * 6) + 1;
+	}
+	roll_result.innerHTML = `Result: ${dice_roll}`;
+	roll_dice_btn.disabled = true;
+	submit_btn.disabled = false;
 }
-
-function roll_die() {
-  // Roll single die, inject text, disable / enable buttons.
-}
-
 
 function sum_checked_values() {
-
+	let sum = 0;
+	for (let i = 0; i < checkboxes.length; i++) {
+		if (!checkboxes[i].disabled && checkboxes[i].checked) {
+			sum += i + 1;
+		}
+	}
+	return sum;
 }
 
 function submit() {
-  // Deal with invalid submission.
-
-  // Deal with valid submission...
-  // Delete text, disable / enable buttons and checkboxes.
-  // Change to rolling one die if appropriate.
+	const sum = sum_checked_values();
+	if (sum != dice_roll) {
+		alert(
+			"The total of the boxes you selected does not match the dice roll. Please make another selection and try again."
+		);
+	} else {
+		unchecked_total -= sum;
+		for (let checkbox of checkboxes) {
+			if (checkbox.checked) {
+				checkbox.disabled = true;
+			}
+		}
+		roll_result.innerHTML = `Result:`;
+		submit_btn.disabled = true;
+		roll_dice_btn.disabled = false;
+	}
 }
 
 function finish() {
-
+	const buttons = document.getElementsByTagName("button");
+	for (let button of buttons) {
+		button.disabled = true;
+	}
+	alert(`Your score is ${unchecked_total}`);
 }
