bab927a99a2a775971cff2753f16f8acc1f65fe7
czhangy <charleszhangmb@gmail.com>
2023-06-02 15:52:28 -0700

    06/02/23 update | COMPTNG 40A: Homework 8 score PHP

--- Misc/COMPTNG-40A/Homeworks/Homework-8/score.php
+++ Misc/COMPTNG-40A/Homeworks/Homework-8/score.php
@@ -1,9 +1,14 @@
 #!/usr/local/bin/php
 <?php
 if (isset($_POST['username']) && isset($_POST['score'])) {
-    $file = fopen('scores.txt', 'w') or die('File could not be opened');
-    $line = "$_POST['username'] $_POST['score']\n";
-    fwrite($file, $line);
+    saveScore();
+}
+
+function saveScore()
+{
+    isset($_POST['username']);
+    $file = @fopen('scores.txt', 'w');
+    fwrite($file, $_POST['username'] .  " " . $_POST['score'] . "\n");
     fclose($file);
 }
 ?>
