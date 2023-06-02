bdaee6784bcb6da2297681fa67a5c4de2a4ea4a0
czhangy <charleszhangmb@gmail.com>
2023-06-02 15:35:23 -0700

    06/02/23 update | COMPTNG 40A: Homework 8 scores JS

---
+++ Misc/COMPTNG-40A/Homeworks/Homework-8/scores.js
@@ -0,0 +1,33 @@
+// Set up click handlers
+document.getElementById("play-again").addEventListener(() => restartGame());
+document.getElementById("start-update").addEventListener(() => startUpdate());
+document.getElementById("stop-update").addEventListener(() => stopUpdate());
+
+let timeoutID = null;
+
+function restartGame() {
+	window.location.href = "welcome.php";
+}
+
+function startUpdate() {
+	stopUpdate();
+	makeGETRequest();
+}
+
+function stopUpdate() {
+	clearTimeout(timeoutID);
+}
+
+function makeGETRequest() {
+	timeoutID = setTimeout(makeGETRequest, 8000);
+	const request = new XMLHttpRequest();
+	request.onload = function () {
+		if (this.status === 200) {
+			document.getElementById("scores").innerHTML += this.responseText
+				.split("\n")
+				.join("<br>");
+		}
+	};
+	request.open("GET", "score.txt?v=" + Math.random());
+	request.send();
+}
